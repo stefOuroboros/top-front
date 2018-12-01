6f3c5fc1fa26dddee47a2a335f0c0979c0dfe7b4
Stéphane Gallois <stephane.gallois89@gmail.com>
2018-12-01 16:26:17 +0100

    Et sinon bien les enums?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BandeauComponentComponent } from './bandeau-component/bandeau-component.component';
 import { AvisComponentComponent } from './avis-component/avis-component.component';
 import { CollegueComponentComponent } from './collegue-component/collegue-component.component';
-import {MatCardModule} from '@angular/material/card';
-import {CSSCarouselComponent} from './carousel-component/carousel-component.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatCarouselModule } from '@ngmodule/material-carousel';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
     BandeauComponentComponent,
     AvisComponentComponent,
     CollegueComponentComponent,
-    CSSCarouselComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatCarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models.ts
+++ src/app/models.ts
@@ -1,13 +1,18 @@
-export class Collegue {
-   nom: string;
-   score: number;
-   photos: string[];
+import { AnimationRenderer } from '@angular/platform-browser/animations/src/animation_renderer';
 
-   constructor() {
-     this.nom = 'thomas';
-     this.score = 10;
-     this.photos = ['https://les-alpages.fr/wp-content/uploads/2014/08/tomme-de-savoie-acheter-fromage.jpg',
-     '../assets/giphy.gif'];
-   }
+export class Collegue {
+  nom: string;
+  score: number;
+  photos: string[];
+  enum Avis {AIMER, DETESTER};
+
+  constructor() {
+    this.enum = Avis;
+    this.nom = 'thomas';
+    this.score = 10;
+    this.photos = ['https://les-alpages.fr/wp-content/uploads/2014/08/tomme-de-savoie-acheter-fromage.jpg',
+      '../assets/giphy.gif'];
+    
+  }
 
 }
